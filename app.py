95d8fe456ecdd3f96bc11b802e44c65bdda9d474
iratcoindo <iratco.indo@gmail.com>
2026-03-28 12:33:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ st.set_page_config(
 # HEADER
 col1, col2 = st.columns([8,2])
 with col1:
-    st.title("iRATco TrackR")
-    st.markdown("<span style='font-size:16px;color:gray;'>version 1.1.0</span>", unsafe_allow_html=True)
+    st.title("")
+    st.markdown()
 with col2:
     st.image("logo_iratco.png", width=250)
     
